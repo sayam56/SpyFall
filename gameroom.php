7c5db6c3c6dacf34501e529399e99b7f8e86037a
Fatema78 <63551942+Fatema78@users.noreply.github.com>
2020-04-21 01:53:12 +0600

    Add files via upload

---
+++ gameroom.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="author" content="Ali Iktider Sayam">
+	<meta name="description" content="Online Health & Fitness Guide">
+	<meta name="keywords" content="gym,fitness,Healthy,Health">
+	<title>SpyFall</title>
+	<link rel="stylesheet" href="bootstrap-4.3.1-dist/css/bootstrap.min.css">
+
+	<link href="gameroom.css" rel="stylesheet">
+	
+	<link rel="icon" href="res/logo.ico">
+	
+	
+</head>
+
+<body> 
+
+		<div class="container">
+			
+			<div class="outer">
+
+				</div>
+			</div>
+</body>
